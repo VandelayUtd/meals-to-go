197bc7ff7d35f5d164287a02dd5bbccccb1c7c5f
VandelayUtd <drewhadley.guitar@gmail.com>
2022-03-17 16:20:10 -0400

    initial app layout

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { getCurrentTimestamp } from 'react-native/Libraries/Utilities/createPerformanceLogger';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={ styles.searchBar }>
+          <Text>Search</Text>
+        </View>
+        <View style={ styles.listView }>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  searchBar: {
+    padding: 16,
+    backgroundColor: '#008000', 
+    marginTop: StatusBar.currentHeight,
   },
+
+  listView: { 
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#0000FF',
+  },
+
 });
