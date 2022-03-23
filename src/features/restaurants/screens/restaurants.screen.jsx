71d9530d9eb87f47a51c62b9c95ffa164306f0b9
VandelayUtd <drewhadley.guitar@gmail.com>
2022-03-23 10:02:25 -0400

    added flatlist component to accomodated multiple restaurants

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,9 +1,11 @@
 import React from 'react'; 
-import { Text, View, SafeAreaView, StatusBar } from 'react-native';
+import {  SafeAreaView, StatusBar, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
+
 
 const SafeArea = styled(SafeAreaView)`
     flex: 1;
@@ -14,20 +16,28 @@ const SearchContainer = styled.View`
     padding: ${props => props.theme.space[3]};
 `;
 
-const RestaurauntListContainer = styled.View`
-    flex: 1;
-    padding: ${props => props.theme.space[3]};
-    background-color: ${props => props.theme.colors.bg.primary};
-`;
+const RestaurantList = styled(FlatList.attrs({
+    contentContainerStyle: {
+        padding:16
+    }
+}))``
+
 
 export const RestaurantsScreen = () => (
     <SafeArea >
         <SearchContainer>
             <Searchbar />
         </SearchContainer>
-        <RestaurauntListContainer>
-            <RestaurantInfoCard />
-        </RestaurauntListContainer>
+        <FlatList 
+            data={[{name: 1}, {name: 2}, {name: 3}, {name: 4}]}
+            renderItem={() => 
+                <Spacer position='bottom' size='large'>
+                    <RestaurantInfoCard />
+                </Spacer>
+                }
+            keyExtractor={(item) => item.name}
+            contentContainerStyle={{padding: 20}}
+        />
     </SafeArea>
 );
 
