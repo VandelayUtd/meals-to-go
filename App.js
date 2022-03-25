f0362bbde42270db586c959fa20ab126f9206aab
VandelayUtd <drewhadley.guitar@gmail.com>
2022-03-25 15:30:18 -0400

    debugged some code and fixed some dependencies

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react'; 
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 
 import {
   useFonts as useOswald,
@@ -9,8 +13,19 @@ import {
 import { useFonts as useLato, Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
 
 import { theme } from './src/infrastructure/theme';
-
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+// import { SafeArea } from "./src/components/utility/safe-area.component";
+
+
+const Tab = createBottomTabNavigator();
+
+// function MyStacks() { 
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name='Home' component={HomeScreen} />
+//     </Tab.Navigator>
+//   )
+// }
 
 export default function App() {
 
@@ -30,7 +45,11 @@ export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
-      <RestaurantsScreen />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.screen name='Restaurants' component={RestaurantScreen}/>
+        </Tab.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -16,11 +16,11 @@ const SearchContainer = styled.View`
     padding: ${props => props.theme.space[3]};
 `;
 
-const RestaurantList = styled(FlatList.attrs({
+const RestaurantList = styled(FlatList).attrs({
     contentContainerStyle: {
         padding:16
     }
-}))``
+})``
 
 
 export const RestaurantsScreen = () => (
