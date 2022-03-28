d51f19a18429308f8906f58f0f9ef07734e46006
VandelayUtd <drewhadley.guitar@gmail.com>
2022-03-28 15:34:58 -0400

    added searchbar to bottom with icons and functional route changes

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react'; 
+import React from 'react';
+import { Text } from 'react-native'; 
 import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Ionicons } from '@expo/vector-icons';
 
 import {
   useFonts as useOswald,
@@ -14,18 +15,29 @@ import { useFonts as useLato, Lato_400Regular, Lato_700Bold } from "@expo-google
 
 import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
-// import { SafeArea } from "./src/components/utility/safe-area.component";
+import { SafeArea } from './src/components/utility/safe-area.component';
 
 
 const Tab = createBottomTabNavigator();
 
-// function MyStacks() { 
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name='Home' component={HomeScreen} />
-//     </Tab.Navigator>
-//   )
-// }
+const TAB_ICON = {
+  Restaurants: 'md-restaurant', 
+  Map: 'md-map',
+  Settings: 'md-settings',
+}
+
+const Settings = () => <SafeArea><Text>Settings</Text></SafeArea>
+const Map = () => <SafeArea><Text>Map</Text></SafeArea>
+
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name]
+  return {
+    tabBarIcon: ({size, color}) => (
+      <Ionicons name={iconName} size={size} color={color}/>
+    ),
+  }
+}
 
 export default function App() {
 
@@ -46,12 +58,20 @@ export default function App() {
     <>
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.screen name='Restaurants' component={RestaurantScreen}/>
+        <Tab.Navigator 
+          screenOptions={createScreenOptions} 
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name='Restaurants' component={RestaurantsScreen}/>
+          <Tab.Screen name='Map' component={Map}/>
+          <Tab.Screen name='Settings' component={Settings}/>
         </Tab.Navigator>
       </NavigationContainer>
     </ThemeProvider>
-      <ExpoStatusBar style="auto" />
+      <ExpoStatusBar style='auto' />
     </>
   );
 }
--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,16 +1,12 @@
 import React from 'react'; 
-import {  SafeAreaView, StatusBar, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
+import { SafeArea } from '../../../components/utility/safe-area.component'
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 import { Spacer } from '../../../components/spacer/spacer.component';
 
-
-const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
-`;
 
 const SearchContainer = styled.View`
     padding: ${props => props.theme.space[3]};
@@ -28,7 +24,7 @@ export const RestaurantsScreen = () => (
         <SearchContainer>
             <Searchbar />
         </SearchContainer>
-        <FlatList 
+        <RestaurantList 
             data={[{name: 1}, {name: 2}, {name: 3}, {name: 4}]}
             renderItem={() => 
                 <Spacer position='bottom' size='large'>
@@ -36,7 +32,6 @@ export const RestaurantsScreen = () => (
                 </Spacer>
                 }
             keyExtractor={(item) => item.name}
-            contentContainerStyle={{padding: 20}}
         />
     </SafeArea>
 );
