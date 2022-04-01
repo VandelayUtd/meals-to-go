de42d7c27c1ac29bbc5b3be6da0c00e40fdaa626
VandelayUtd <drewhadley.guitar@gmail.com>
2022-04-01 16:11:18 -0400

    app can now render mock restaurant data

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -6,11 +6,40 @@ import { restaurantsRequest, restaurantsTransform } from './restaurants.service'
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+
+    const [restaurants, setRestaurants] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const retrieveRestaurants = () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            restaurantsRequest()
+            .then(restaurantsTransform)
+            .then(results => {
+                setIsLoading(false)
+                setRestaurants(results);
+            })
+            .cach(err => {
+                setIsLoading(false);
+                setError(err);
+            })
+        }, 2000)
+    };
+    
+    useEffect(() => {
+        retrieveRestaurants();
+    }, []);
+
+console.log(restaurants)
+
    return (
        <RestaurantsContext.Provider
-        value={{
-            restaurants: [1, 2, 3],
-        }}
+            value={{
+                restaurants,
+                isLoading,
+                error,
+            }}
        >
            {children}
        </RestaurantsContext.Provider>
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,4 @@
-import { mocks } from './mock';
+import { mockImages, mocks } from './mock';
 import camelize from 'camelize';
 
 export const restaurantsRequest = (location = "43.653225,-79.383186") => {
@@ -13,6 +13,9 @@ export const restaurantsRequest = (location = "43.653225,-79.383186") => {
 
 export const restaurantsTransform = ({results = []}) => {
     const mappedResults = results.map((restaurant) => {
+        restaurant.photos = restaurant.photos.map(p => {
+            return mockImages[Math.ceil(Math.random() *( mockImages.length - 1))]
+        })
         return {
             ...restaurant,
             isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
