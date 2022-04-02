a1038d2e29decbb52dfc3281278688267b50f7e1
VandelayUtd <drewhadley.guitar@gmail.com>
2022-04-02 12:02:10 -0400

    added loading animation

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'; 
-import { Searchbar } from 'react-native-paper';
+import { Searchbar, ActivityIndicator, Colors } from 'react-native-paper';
 import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
@@ -19,6 +19,15 @@ const RestaurantList = styled(FlatList).attrs({
     }
 })``
 
+const Loading = styled(ActivityIndicator)`
+    margin-left: -25x;
+`
+
+const LoadingContainer = styled.View`
+    position: absolute;
+    top:50%;
+    left:50%;
+`
 
 export const RestaurantsScreen = () => {
     
@@ -26,6 +35,11 @@ export const RestaurantsScreen = () => {
     
     return (
         <SafeArea >
+        {isLoading && (
+            <LoadingContainer>
+                <Loading size={50} animating={true} color={Colors.blue300} />
+            </LoadingContainer>
+        )}
             <SearchContainer>
                 <Searchbar />
             </SearchContainer>
